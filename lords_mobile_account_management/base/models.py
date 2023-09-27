f449d554bedd7fbc0193982f41b068fc74fcfb6a
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2023-09-27 23:59:47 +0800

    add lm accounts model

--- lords_mobile_account_management/base/models.py
+++ lords_mobile_account_management/base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class LMAccount(models.Model):
+    id = models.AutoField(primary_key=True)
+    IGN = models.CharField(max_length=20)
+    reset_time = models.IntegerField()
+    email = models.EmailField(max_length=255)
+
+    class Meta:
+        db_table = "lm_account"
