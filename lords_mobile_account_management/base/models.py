654d9a567b7433303b734abb0f548e1da5aeb8b0
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2023-10-15 23:43:34 +0800

    add create account feature, will finish later

--- lords_mobile_account_management/base/models.py
+++ lords_mobile_account_management/base/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class LMAccount(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, auto_created=True)
     IGN = models.CharField(max_length=20)
     reset_time = models.IntegerField()
     email = models.EmailField(max_length=255)
--- lords_mobile_account_management/lm_accounts/views.py
+++ lords_mobile_account_management/lm_accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from base.models import LMAccount
 
 @login_required
 def home(request):
@@ -7,3 +8,12 @@ def home(request):
         "home_message":"accounts page",
     }
     return render(request,'lm_accounts/home.html',context)
+
+@login_required
+def createAccount(request):
+    LMAccount.IGN = ""
+    LMAccount.reset_time = ""
+    LMAccount.email = ""
+    # IGN = models.CharField(max_length=20)
+    # reset_time = models.IntegerField()
+    # email = models.EmailField(max_length=255)
