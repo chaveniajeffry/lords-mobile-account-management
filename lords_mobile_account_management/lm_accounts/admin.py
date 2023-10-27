689c5c0c598c00c0adbdddb2ce8f9e91d47370d5
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2023-10-27 21:13:33 +0800

    add lm bag in admin

--- lords_mobile_account_management/lm_accounts/admin.py
+++ lords_mobile_account_management/lm_accounts/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import LMUserAccount
+from .models import LMUserAccount,LMUserBag,LMUserBagChest,LMUserBagCombat,LMUserBagResources,LMUserBagSpeedUp,LMUserBagUnique
 # Register your models here.
 admin.site.register(LMUserAccount)
+admin.site.register(LMUserBagChest)
+admin.site.register(LMUserBagCombat)
+admin.site.register(LMUserBagResources)
+admin.site.register(LMUserBagSpeedUp)
+admin.site.register(LMUserBagUnique)
