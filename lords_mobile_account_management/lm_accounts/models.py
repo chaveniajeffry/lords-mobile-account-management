4c22aaf16acc2e75d0504b8a47aea261893678cd
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2023-10-28 18:00:04 +0800

    add research model

--- lords_mobile_account_management/lm_accounts/models.py
+++ lords_mobile_account_management/lm_accounts/models.py
@@ -70,3 +70,153 @@ class LMUserBag(models.Model):
 
     class Meta:
         db_table = "lm_user_bag"
+
+class LMUserResearchEconomy(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_economy"
+
+class LMUserResearchEconomy(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_economy"
+
+class LMUserResearchDefense(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_defense"
+
+class LMUserResearchMilitary(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_military"
+
+class LMUserResearchMonsterHunt(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_monster_hunt"
+
+class LMUserResearchMonsterHunt(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_monster_hunt"
+
+class LMUserResearchUpgradeDefenses(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_upgrade_defenses"
+
+class LMUserResearchUpgradeMilitary(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_upgrade_military"
+
+class LMUserResearchArmyLeadership(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_army_leadership"
+
+class LMUserResearchMilitaryCommand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_military_command"
+
+class LMUserResearchFamiliar(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_familiar"
+
+class LMUserResearchFamiliarBattles(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_familiar_battles"
+
+class LMUserResearchSigil(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_sigil"
+
+class LMUserResearchWonderBattles(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_wonder_battles"
+
+class LMUserResearchGear(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_gear"
+
+class LMUserResearchAdvancedWonderBattles(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    level = models.IntegerField()
+
+    class Meta:
+        db_table = "lm_user_research_advanced_wonder_battles"
+
+class LMUserResearch(models.Model):
+    id = models.AutoField(primary_key=True)
+    account_id = models.ForeignKey(LMUserAccount, on_delete=models.CASCADE)
+    economy_id = models.ForeignKey(LMUserResearchEconomy, on_delete=models.CASCADE)
+    defense_id = models.ForeignKey(LMUserResearchDefense, on_delete=models.CASCADE)
+    military_id = models.ForeignKey(LMUserResearchMilitary, on_delete=models.CASCADE)
+    monster_hunt_id = models.ForeignKey(LMUserResearchMonsterHunt, on_delete=models.CASCADE)
+    upgrade_defenses_id = models.ForeignKey(LMUserResearchUpgradeDefenses, on_delete=models.CASCADE)
+    upgrade_military_id = models.ForeignKey(LMUserResearchUpgradeMilitary, on_delete=models.CASCADE)
+    army_leadership_id = models.ForeignKey(LMUserResearchArmyLeadership, on_delete=models.CASCADE)
+    military_command_id = models.ForeignKey(LMUserResearchMilitaryCommand, on_delete=models.CASCADE)
+    familiar_id = models.ForeignKey(LMUserResearchFamiliar, on_delete=models.CASCADE)
+    battle_familiar_id = models.ForeignKey(LMUserResearchFamiliarBattles, on_delete=models.CASCADE)
+    sigil_id = models.ForeignKey(LMUserResearchSigil, on_delete=models.CASCADE)
+    wonder_battles_id = models.ForeignKey(LMUserResearchWonderBattles, on_delete=models.CASCADE)
+    gear_id = models.ForeignKey(LMUserResearchGear, on_delete=models.CASCADE)
+    advanced_wonder_battles_id = models.ForeignKey(LMUserResearchAdvancedWonderBattles, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "lm_user_research"
+
